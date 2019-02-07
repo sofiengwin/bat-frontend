cec5e1c450f763bf5e3091b1cf8849a24debbae7
Ogbara Godwin <godwin.onisofien@andela.com>
2019-02-07 12:50:16 +0100

    Updates

---
+++ precache-manifest.72a5fc896293123f9f494821638bd34d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa3e4c6d792b73cfed2c",
+    "url": "/static/css/main.1033c795.chunk.css"
+  },
+  {
+    "revision": "fa3e4c6d792b73cfed2c",
+    "url": "/static/js/main.fa3e4c6d.chunk.js"
+  },
+  {
+    "revision": "9826277e50fbfffeeabe",
+    "url": "/static/css/1.f92bc72e.chunk.css"
+  },
+  {
+    "revision": "9826277e50fbfffeeabe",
+    "url": "/static/js/1.9826277e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f22c0a38a6a1f9cd590e031f352ca2ee",
+    "url": "/index.html"
+  }
+];
