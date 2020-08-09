30065f22db36a18f9dcb8e4885f8caa9cec47965
Ogbara Godwin <godwin.onisofien@andela.com>
2020-08-10 00:28:11 +0100

    add support for foster pattern using context api

---
+++ src/components/Fosterage.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import {useContext, useState} from 'react';
+
+interface IFoster {
+  foster: (component: React.ComponentType) => void;
+  reset: () => void;
+}
+
+const FosterContext = React.createContext<IFoster>({foster: () => null, reset: () => null});
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Fosterage: React.FC<Props> = ({children}) => {
+  const [Component, setComponent] = useState<React.ComponentType | null>(null);
+  const foster = (component: React.ComponentType) => setComponent(component);
+  const reset = () => setComponent(null);
+
+  return (
+    <FosterContext.Provider value={{foster, reset}}>
+      {Component && Component}
+      {children}
+    </FosterContext.Provider>
+  );
+};
+
+export default Fosterage;
+
+export const useFoster = () => {
+  return useContext(FosterContext);
+}
